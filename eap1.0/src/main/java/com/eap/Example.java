0118d35de3ea7b88215c8e5b404a33505e47d627
chuchaochao <704995502@qq.com>
2016-08-07 17:40:37 +0800

    example1

--- eap1.0/src/main/java/com/eap/Example.java
+++ eap1.0/src/main/java/com/eap/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World 哈哈!";
     }
 
     public static void main(String[] args) throws Exception {
